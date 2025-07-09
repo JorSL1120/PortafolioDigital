a6684905c458660bd9a9601833158397b5cbcdf0
Jorge Salas Leybon <146293962+JorSL1120@users.noreply.github.com>
2025-07-08 19:44:36 -0600

    cambiando el diseño

--- docs/main.js
+++
@@ -1,8 +0,0 @@
-// Inicializar AOS (animaciones al hacer scroll)
-document.addEventListener('DOMContentLoaded', function () {
-  AOS.init({
-    duration: 800,
-    easing: 'ease-out-quart',
-    once: true
-  });
-});
